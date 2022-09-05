4424b11ac8203ee8286a4f5d02749a60e1843629
IbrahimSaffi <ibrahimsaffinew@gmail.com>
2022-09-05 10:27:56 +0500

    Completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+  let [images,setImages] = useState([]) 
+  let [page,setPage] =useState(1)
+  let [Q,setQ] = useState("")
+  let inp = useRef(null)
+  function search(q) {
+    if (q.length > 0) {
+      fetch(`https://api.pexels.com/v1/search?query=${q}&per_page=25&page=${page}&size=small`, {
+        method: "GET",
+        headers:{
+          'Content-Type': 'application/json',
+          authorization: "563492ad6f91700001000001f600d4b0691a48db9dc1fb4924dc754d",
+        }
+      }).then(res=>res.json()).then(data=>{
+      setImages(data.photos)
+      setQ(q)
+      })
+    }
+  }
+  function trending(){
+    fetch(`https://api.pexels.com/v1/curated?per_page=25&page=${page}&size=small`, {
+      method: "GET",
+      headers:{
+        'Content-Type': 'application/json',
+        authorization: "563492ad6f91700001000001f600d4b0691a48db9dc1fb4924dc754d",
+      }
+    }).then(res=>res.json()).then(data=>{
+      setImages(data.photos)
+    })
+  }
+  useEffect(()=>{
+    trending()
+  },[])
+  useEffect(()=>{
+  if(Q.length===0){
+      trending()
+  }
+  else{
+    search(Q)
+  }
+  },[page])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app' >
+      <div>
+      <input ref={inp} type="text" required />
+      <button onClick={() => {setPage(1)
+    setQ(inp.current.value)  
+    }
+      } >Search</button>
+      </div>
+      <div className='images'>
+         {images.map((ele,i)=>{
+          return <img key={i} src={ele.src.medium} alt="" />
+        })} 
+      </div>
+     <button onClick={()=>{
+        let currPage = page
+        setPage(currPage+1)
+      }} >Next Page</button>
     </div>
-  );
+  )
 }
 
 export default App;
